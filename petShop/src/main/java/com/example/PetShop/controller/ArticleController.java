fe3c22f374089b1c83b06298c3c2305d7821caf9
Кирилл Фадеев <kirillfadeev@Mac-mini-Kirill.local>
2025-03-13 19:16:00 +0300

    feature-PS-2 Add DB integration, write get endpoint

---
+++ petShop/src/main/java/com/example/PetShop/controller/ArticleController.java
@@ -0,0 +1,23 @@
+package com.example.PetShop.controller;
+
+import com.example.PetShop.repository.Article;
+import com.example.PetShop.service.ArticleService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ArticleController {
+
+    private final ArticleService articleService;
+
+    public ArticleController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
+    @GetMapping
+    public List<Article> getArticles() {
+        return articleService.getArticles();
+    }
+}
