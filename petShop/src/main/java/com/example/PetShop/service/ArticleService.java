8e9d4822ad1c9cc4a2e734064845585a48a79da2
Кирилл Фадеев <kirillfadeev@Mac-mini-Kirill.local>
2025-03-14 17:35:17 +0300

    feature-PS-3 Add post method for adding articles

--- petShop/src/main/java/com/example/PetShop/service/ArticleService.java
+++ petShop/src/main/java/com/example/PetShop/service/ArticleService.java
@@ -3,8 +3,10 @@ package com.example.PetShop.service;
 import com.example.PetShop.repository.Article;
 import com.example.PetShop.repository.ArticleRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ArticleService {
@@ -17,5 +19,13 @@ public class ArticleService {
 
     public List<Article> getArticles() {
         return articleRepository.findAll();
+    }
+
+    public Article createArticle(Article article) {
+        Optional<Article> optionalArticle =  articleRepository.findBySku(article.getSku());
+        if (optionalArticle.isPresent()) {
+            throw new IllegalStateException("Article already exists");
+        }
+        return this.articleRepository.save(article);
     }
 }
